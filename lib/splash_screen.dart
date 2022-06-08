5560dbf597c8c411f56264599095265b45f13690
Nishadatgit <nishadns321@gmail.com>
2022-06-08 13:12:21 +0530

    flutter day 3 final commit

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,78 +1,98 @@
-import 'dart:async';
+// import 'dart:async';
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget {
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     Timer(
+//         Duration(seconds: 3),
+//         () => Navigator.pushReplacement(
+//             context, MaterialPageRoute(builder: (context) => ScreenHome())));
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.pink,
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Image.asset(
+//               'assets/images/avatar.png',
+//               height: 200,
+//               width: 400,
+//             ),
+//             const SizedBox(
+//               height: 10,
+//             ),
+//             const Text(
+//               'Splash Screen',
+//               style: TextStyle(
+//                   color: Colors.white,
+//                   fontSize: 20,
+//                   letterSpacing: 5,
+//                   fontWeight: FontWeight.bold,
+//                   fontStyle: FontStyle.italic),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class ScreenHome extends StatelessWidget {
+//   const ScreenHome({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         leading: const Icon(
+//           Icons.home,
+//           size: 20,
+//         ),
+//         title: const Text(
+//           'Demo App',
+//           style: TextStyle(color: Colors.white),
+//         ),
+//       ),
+//       body: Container(
+//         width: 400,
+//         height: 300,
+//         child: Image.network(
+//             'https://image.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg'),
+//       ),
+//     );
+//   }
+// }
+import 'package:demo_project/home_screen.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<Splash> createState() => _SplashState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-
-   
-    Timer(
-        Duration(seconds: 3),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => ScreenHome())));
-  }
-
+class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.pink,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/avatar.png',
-              height: 200,
-              width: 400,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'Splash Screen',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  letterSpacing: 5,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ScreenHome extends StatelessWidget {
-  const ScreenHome({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(
-          Icons.home,
-          size: 20,
-        ),
-        title: const Text(
-          'Demo App',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Container(
-        width: 400,
-        height: 300,
-        child: Image.network(
-            'https://image.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg'),
-      ),
+    return SplashScreen(
+      seconds: 3,
+      navigateAfterSeconds: HomeScreen2(),
     );
   }
 }
