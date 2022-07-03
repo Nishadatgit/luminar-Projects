476a131bf022165684d52a8fe99cf9e08bc8fbe6
Nishadatgit <nishadns321@gmail.com>
2022-07-03 23:45:01 +0530

    profile ui completed

--- lib/assignments/profile ui/profile.dart
+++ lib/assignments/profile ui/profile.dart
@@ -1,10 +1,6 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:line_icons/line_icons.dart';
 
 class ProfileUi extends StatelessWidget {
   const ProfileUi({Key? key}) : super(key: key);
@@ -20,7 +16,7 @@ class ProfileUi extends StatelessWidget {
         child: ListView(
           children: [
             Container(
-              padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
               height: 40,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -52,11 +48,11 @@ class ProfileUi extends StatelessWidget {
                   boxShadow: [
                     BoxShadow(
                         color: Colors.white,
-                        offset: Offset(10, 10),
+                        offset: Offset(20, 10),
                         blurRadius: 25),
                     BoxShadow(
                         color: Colors.white,
-                        offset: Offset(-10, -10),
+                        offset: Offset(-20, -10),
                         blurRadius: 17),
                     BoxShadow(
                         color: Colors.white,
@@ -118,11 +114,121 @@ class ProfileUi extends StatelessWidget {
               ),
             ),
             Container(
-              height: 50,
-              margin: EdgeInsets.only(left: 40, right: 40, top: 30),
-              color: Colors.blue,
+              height: 60,
+              margin: const EdgeInsets.only(
+                left: 30,
+                right: 30,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        'Mobile App Developer And Open',
+                        style: TextStyle(
+                            fontSize: 23, fontWeight: FontWeight.w500),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Source Enthusisastic',
+                        style: TextStyle(
+                            fontSize: 23, fontWeight: FontWeight.w500),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 35, right: 35),
+              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery.of(context).size.width * 0.2,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: const [
+                    ProfileTile(
+                      txt: 'Privacy',
+                      icon: Icon(
+                        Icons.privacy_tip,
+                        color: Colors.black,
+                      ),
+                    ),
+                    ProfileTile(
+                      txt: 'Purchase history',
+                      icon: Icon(
+                        Icons.history,
+                        color: Colors.black,
+                      ),
+                    ),
+                    ProfileTile(
+                      txt: 'Help and Support',
+                      icon: Icon(
+                        Icons.help,
+                        color: Colors.black,
+                      ),
+                    ),
+                    ProfileTile(
+                      txt: 'Settings',
+                      icon: Icon(
+                        Icons.settings,
+                        color: Colors.black,
+                      ),
+                    ),
+                    ProfileTile(
+                      txt: 'Invite a friend',
+                      icon: Icon(
+                        Icons.person_add,
+                        color: Colors.black,
+                      ),
+                    ),
+                    ProfileTile(
+                      txt: 'Logout',
+                      icon: Icon(
+                        EvaIcons.logOut,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileTile extends StatelessWidget {
+  const ProfileTile({
+    Key? key,
+    required this.txt,
+    required this.icon,
+  }) : super(key: key);
+  final String txt;
+  final Icon icon;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.grey[300],
+      ),
+      child: ListTile(
+        dense: true,
+        title: Text(
+          txt,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        leading: icon,
+        trailing: const Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.black,
         ),
       ),
     );
